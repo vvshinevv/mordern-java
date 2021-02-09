996ba5f7a4545baea644bc2dbd08eda049f17c2a
최홍희 <vvshinevv@nhntoast.com>
2021-02-09 21:47:19 +0900

    first commit

---
+++ src/main/java/cc/hongs/example/WhyJava8.java
@@ -0,0 +1,18 @@
+package cc.hongs.example;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class WhyJava8 {
+
+    public static void main(String[] args) {
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        final String result = numbers.stream()
+                .map(String::valueOf)
+                .collect(joining(" : "));
+
+        System.out.println(result);
+    }
+}
